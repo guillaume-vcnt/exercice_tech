fc81c32f354f40810115f6d8aa81e6fde2014aa0
Guillaume <guillaume.vincent1138@gmail.com>
2025-04-30 13:24:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Exercice clavier de téléphone (Entretien tech)
+// Keyboard
 
-// Je déclare une variable pour simuler un input-frontend qui contient un mot tapé par l'utilisateur (ici une chaîne String avec le mot "Banane")
+// Je déclare une variable pour simuler un input-frontend qui contient un mot tapé par l'utilisateur (ici une chaîne String avec le mot "Banane").
 
 const inputUser = "Banane";
 
-// J'appelle la fonction avec la valeur inscrite dans l'inputUser et j'ajoute à mon console.log une variable dynamique ${} qui affichera le mot de l'utilisateur et sa conversion/clavier
-// Banane = touche_2 [2] touche_2 [1] touche_6 [2] touche_2 [1] touche_6 [2] touche_3 [2]
+// J'appelle la fonction avec la valeur inscrite dans l'inputUser et j'ajoute à mon console.log une variable dynamique ${} qui affichera le mot de l'utilisateur et sa conversion/clavier.
+// Exemple : Banane = touche_2 [2] touche_2 [1] touche_6 [2] touche_2 [1] touche_6 [2] touche_3 [2]
 
 console.log(`${inputUser} = ${convertStringToKeyboard(inputUser)}`);
 
@@ -13,8 +13,8 @@ console.log(`${inputUser} = ${convertStringToKeyboard(inputUser)}`);
 
 function convertStringToKeyboard(word) {
   
-  // Je déclare une variable keyboard qui contient une map objet clé/valeur où chaque clé représente une touche
-  // Chaque valeur de la clé est un tableau contenant les lettres correspondant à la touche
+  // Je déclare une variable "keyboard" qui contient un objet clé/valeur où chaque clé représente une touche.
+  // Chaque valeur de la clé est un tableau contenant les lettres correspondant à la touche.
 
   const keyboard = {
     touche_1: [],
@@ -29,10 +29,10 @@ function convertStringToKeyboard(word) {
     touche_0: [],
   };
 
-  // Je vérifie que je récupère bien les valeurs de la touche 1
+  // Je vérifie que je récupère bien les valeurs de la touche 1.
   // console.log(keyboard.touche_1);
 
-  // Je déclare une variable vide pour stocker ma future chaîne convertie
+  // Je déclare une variable "result" vide pour stocker ma future chaîne convertie.
 
   let result = "";
 
@@ -40,23 +40,23 @@ function convertStringToKeyboard(word) {
 
   for (let i = 0; i < word.length; i++) {
 
-    // Je passe la lettre en minuscule avec .toLowerCase() au cas où l'inputUser contiendrait des majuscules et supprime les espaces vide avec .trim() par sécurité (exemple " Banane ")
+    // Je passe la lettre en minuscule avec .toLowerCase() au cas où l'inputUser contiendrait des majuscules et je supprime les espaces vide avec .trim() par sécurité (exemple " Banane ").
 
     const letter = word[i].toLowerCase().trim();
 
-    // Je vérifie si j'ai bien mes lettres dans ma variable
+    // Je vérifie si j'ai bien mes lettres dans ma variable.
     // console.log(letter);
 
-    // Je parcours chaque clé de mon objet keyboard (touche_1, touche_2, etc...) avec une boucle for-in et je stocke les clés dans une variable "key"
-    // La boucle for in est utile pour parcourir les clés d'un objet facilement en JavaScript mais il y a d'autres méthodes possibles...
+    // Je parcours chaque clé de mon objet "keyboard" (touche_1, touche_2, etc...) avec une boucle for-in et je stocke les clés dans une variable "key"
+    // La boucle for in est utile ici pour parcourir les clés de mon objet facilement.
 
     for (let key in keyboard) {
       
-      // Avec une condition if je vérifie si la lettre est bien présente dans l'objet avec l'aide de .includes() et j'ajoute la touche et l'index de la lettre à la variable result
+      // Avec une condition if je vérifie si la lettre est bien présente dans objet "keyboard" avec l'aide de .includes() et j'ajoute la touche et l'index de la lettre à la variable "result".
 
       if (keyboard[key].includes(letter)) {
         
-        // Je stocke l'index de la lettre dans ma variable index avec la fonction .indexOf()
+        // Je stocke l'index de la lettre dans la variable "index" avec la fonction .indexOf()
 
         const index = keyboard[key].indexOf(letter);
 
